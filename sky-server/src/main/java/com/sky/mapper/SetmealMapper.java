b7ee76be3f76cc017f224c29f84e7bd37a415fbd
FEEESH <2825295343@qq.com>
2026-03-24 13:39:42 +0800

    feat: 添加缓存菜品功能

--- sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
@@ -1,6 +1,8 @@
 package com.sky.mapper;
 
+import com.sky.annotation.AutoFill;
 import com.sky.entity.Setmeal;
+import com.sky.enumeration.OperationType;
 import com.sky.vo.DishItemVO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -29,4 +31,12 @@ public interface SetmealMapper {
     List<DishItemVO> getDishItemBySetmealId(Long setmealId);
 
     List<Setmeal> list(Setmeal setmeal);
+
+    /**
+     * 根据id修改套餐
+     *
+     * @param setmeal
+     */
+    @AutoFill(OperationType.UPDATE)
+    void update(Setmeal setmeal);
 }
